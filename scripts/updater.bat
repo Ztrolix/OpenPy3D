9fde644823fca23f0d6b8a527137bdd1160ab943
Ztrolix <119396780+Ztrolix@users.noreply.github.com>
2023-08-21 17:17:00 +1000

    Add files via upload

--- scripts/updater.bat
+++ scripts/updater.bat
@@ -2,6 +2,9 @@
 title OpenPy3D - Console
 color 0f
 
+py -m ensurepip --upgrade  --user
+py get-pip.py --user
+
 pip install --upgrade pip --user
 pip install pygame --user
 pip install PyOpenGL --user
@@ -9,3 +12,15 @@ pip install tk --user
 pip install pillow --user
 pip install tqdm --user 
 pip install alive-progress --user
+pip install pathlib --user
+pip install setuptools --user
+pip install zipp --user
+pip install urllib3 --user
+pip install requests --user
+pip install colorama --user
+pip install grapheme --user
+pip install certifi --user
+pip install idna --user
+pip install charset-normalizer --user
+pip install logging --user
+pip install noise --user
